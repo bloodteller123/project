0b5d5914fd3f0f46f2e7f8ad482643709a107737
bloodteller123 <chenzihan708@gmail.com>
2019-07-11 22:01:37 -0400

    challenge example

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+import Challenge from './container/Challenge';
+
+function App() {
+  return (
+    <div>
+      <Challenge number={0}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/code/testCode.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const testCode = [
+    {
+        challengeTitel:'Challenge ',
+        code: 
+        `public class test {
+    public static void main(String[] args) {
+        System.out.println(Math.max(3,8));
+    }    
+}`,
+        description:'What is the result of 3 + 5 ? Please print in using system library',
+        hint: "no hints, hold my farts",
+        expectedResult: "8\n"
+        
+    },
+];
+
+export {testCode};
